d2a3136f19c6e8502a0a8fc3f3f382f5dcd1322f
Raymond Micheau <raymicheau@yahoo.ca>
2016-11-24 00:12:37 -0500

    Added Dependencies + Model Loader
    Model Loader added, not sure if it works yet. Still gotta mess with it.

--- NYXEngine/OpenGLRenderer.h
+++ NYXEngine/OpenGLRenderer.h
@@ -30,6 +30,5 @@ public:
 	void PreRender();
 	void PostRender();
 	void renderPrimitive(PRIMITIVETYPE prim);
-	bool loadOBJ(const char* path, std::vector<glm::vec3> &out_vertices, std::vector<glm::vec2> &out_uvs, std::vector<glm::vec3> &out_normals);
 };
 
